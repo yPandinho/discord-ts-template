e8df91e51853cf620c91728221893f7171166b35
Everton <104120062+yPandinho@users.noreply.github.com>
2026-05-12 22:45:59 -0300

    refactor: move interaction handler to explicit function

--- src/handlers/interaction.ts
+++ src/handlers/interaction.ts
@@ -1,27 +1,28 @@
-import { Events, type Interaction } from 'discord.js';
-import client from '../index.js';
+import { Client, Events, type Interaction } from 'discord.js';
 
-client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-    if (interaction.isChatInputCommand()) {
-        const command = client.slash_commands.get(interaction.commandName);
-        if (!command) return;
-        return execute(interaction.commandName, () => command.run(client, interaction));
-    }
+export function loadInteractions(client: Client): void {
+    client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+        if (interaction.isChatInputCommand()) {
+            const command = client.slash_commands.get(interaction.commandName);
+            if (!command) return;
+            return execute(interaction.commandName, () => command.run(client, interaction));
+        }
 
-    if (interaction.isUserContextMenuCommand()) {
-        const command = client.user_commands.get(interaction.commandName);
-        if (!command) return;
-        return execute(interaction.commandName, () => command.run(client, interaction))
-    }
+        if (interaction.isUserContextMenuCommand()) {
+            const command = client.user_commands.get(interaction.commandName);
+            if (!command) return;
+            return execute(interaction.commandName, () => command.run(client, interaction))
+        }
 
-    if (interaction.isMessageContextMenuCommand()) {
-        const command = client.message_commands.get(interaction.commandName);
-        if (!command) return;
-        return execute(interaction.commandName, () => command.run(client, interaction));
-    }
-});
+        if (interaction.isMessageContextMenuCommand()) {
+            const command = client.message_commands.get(interaction.commandName);
+            if (!command) return;
+            return execute(interaction.commandName, () => command.run(client, interaction));
+        }
+    });
+}
 
-async function execute(commandName: string , run: () => Promise<void>): Promise<void> {
+async function execute(commandName: string, run: () => Promise<void>): Promise<void> {
     try {
         await run();
     } catch (err) {
